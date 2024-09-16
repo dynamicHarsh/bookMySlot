ad1fbde881ad3118294194c6337164048408aa87
Harsh Sonkar <2140154@sliet.ac.in>
2024-09-16 17:37:49 +0530

    updated cors issue

--- server/index.js
+++ server/index.js
@@ -1,43 +1,40 @@
-const express=require('express');
-const morgan=require('morgan');
-const dotenv=require('dotenv');
-const cookieParser=require('cookie-parser');
-const connectDB=require('./config/db');
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
 const userRouter = require('./routes/userRoutes');
-const path=require('path');
-const cors=require('cors');
-// const adminRouter=require('./routes/adminRoutes');
+const path = require('path');
+const cors = require('cors');
+
 dotenv.config();
 connectDB();
 
-const app=express();
+const app = express();
 
-app.use(cors({
-  origin: ["https://bookmyslot.vercel.app/"],
-  methods: ["POST","GET"],
-  credentials: true
-}))
+const corsOptions = {
+  origin: 'https://bookmyslot.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
 
-
-
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
+app.use('/user', userRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
+}
 
-app.use('/user',userRouter);
-// app.use('/admin',adminRouter);
+app.options('*', cors(corsOptions));
 
-const port=process.env.PORT || 8080;
-
-
-app.use(express.static(path.join(__dirname,"./client/build")))
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
-});
-
-app.listen(port,()=>{
-    console.log(`Server is listening at port ${port} in ${process.env.NODE_MODE} mode`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${process.env.NODE_ENV} mode`);
 });
