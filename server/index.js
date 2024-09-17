e6f121927f821163b7cbbf40cc37e50ffc973843
Harsh Sonkar <2140154@sliet.ac.in>
2024-09-17 11:12:10 +0530

    updated urls

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
-const express=require('express');
-const morgan=require('morgan');
-const dotenv=require('dotenv');
-const cookieParser=require('cookie-parser');
-const connectDB=require('./config/db');
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
 const userRouter = require('./routes/userRoutes');
-const path=require('path');
+const path = require('path');
 const cors = require('cors');
-// const adminRouter=require('./routes/adminRoutes');
+
 dotenv.config();
 connectDB();
 
-const app=express();
+const app = express();
 
 app.use(cors({
-  origin: 'https://bookmyslot.vercel.app/', // Replace with the domain you want to allow
+  origin: 'https://bookmyslot.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // If you need to support cookies and authentication
+  credentials: true
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
+app.use('/user', userRouter);
 
+app.options('*', cors({
+  origin: 'https://bookmyslot.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
-app.use('/user',userRouter);
-// app.use('/admin',adminRouter);
-
-const port=process.env.PORT || 8080;
-
-
-app.use(express.static(path.join(__dirname,"./client/build")))
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-app.listen(port,()=>{
-    console.log(`Server is listening at port ${port} in ${process.env.NODE_MODE} mode`);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port} in ${process.env.NODE_MODE} mode`);
 });
