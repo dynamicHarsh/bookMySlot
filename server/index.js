a792f32f2a047607a45ffeebdeb731fbc85bee16
Harsh Sonkar <2140154@sliet.ac.in>
2024-09-17 11:43:51 +0530

    updated login

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(morgan('dev'));
 app.use('/user', userRouter);
 
 app.options('*', cors({
-  origin: 'https://bookmyslot.vercel.app',
+  origin: 'https://bookmyslot.vercel.app/login',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
